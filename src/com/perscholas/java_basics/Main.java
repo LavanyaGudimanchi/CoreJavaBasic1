2c8f31627788152dfbfe0272d44bda9298be2ea9
Lavanya Gudimanchi <srilavanya.gudimanchi@gmail.com[D[D[D[D[D[D[D[D[D[D0817@gma[C[Cail.com>
2022-02-18 16:13:20 -0500

    Main excercises

---
+++ src/com/perscholas/java_basics/Main.java
@@ -0,0 +1,28 @@
+package com.perscholas.java_basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //1.
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+         //2.
+        System.out.println(z);
+        double x1 = 10.5;
+        double y1 = 20.5;
+        double z1 = x1 + y1;
+        System.out.println(z1);
+         //3.
+        int a = 20;
+        double b = 20.5;
+        double c = a+b;
+        System.out.println(c);
+
+
+
+
+
+    }
+}
